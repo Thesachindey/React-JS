5a668aa265a3b070ab4c89958d265d4e233b4e63
Thesachindey <sachinscode24@gmail.com>
2024-09-27 10:02:17 +0600

    add

---
+++ Lecture 04 Passing PROPS as Children and Functions/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import './components/card'
+
+function App() {
+
+  return (
+    <>
+      <Card name="sachin" >
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>Lorem, ipsum.</p>
+        <p>Lorem ipsum dolor consectetur . Eligendi, cum?</p>
+      </Card>
+    </>
+  )
+}
+
+export default App
