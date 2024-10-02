0548058bf457e8859663d768ba26e6d8dcba8d35
Thesachindey <sachinscode24@gmail.com>
2024-10-02 18:53:51 +0600

    add

--- Lecture 08 useEffect HOOK in depth/src/App.jsx
+++ Lecture 08 useEffect HOOK in depth/src/App.jsx
@@ -2,28 +2,72 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
   const [Count, setCount] = useState(0);
+  const [Total, setTotal] = useState(1);
 
   //first -> side-effect function
   //second -> clean-up function
   //third -> comma separated dependenci list; 
+  //[]-> dependenci jotobar ---> change/update hoby toto bar ayta work korby.
 
   //useEffect()->hook-yr onyk gulo variation roycy.
 
-  //Variation:1
+
+
+  //Variation: 1
   //runs on every render
-  useEffect(() => {
-    alert("I will run on each render")
-  })
+  // useEffect(() => {
+  //   alert("I will run on each/every render")
+  // })
+
+  //Variation: 2
+  //that runs on only first render
+  // useEffect(() => {
+  //   alert("I will run on only first render ")
+  // }, []);
+   //if we pass 'count' as a 'depen.' then it work as a variation-1.
+
+//variation: 3
+// useEffect(()=>{
+//   alert("I will run every time when count is update")
+// },[Count]) //count 0 is also a count update
+
+//variation: 4
+//multiple dependencies
+// useEffect(() => {
+//   alert("I will run every time when Count/total is update")
+// }, [Count,Total]);
+
+//variation: 5
+//aibar add korbo cleanup function
+useEffect(() => {
+  alert("New count is updated")
+
+  // CleanUp Function 
+  return () => {
+   alert("Old count is unmounted from UI")
+  }
+}, [Count])
+//Aikhany "Count-state" soraly(Unmount-holy) clean-up function kaj korby.
+
 
   function handaleClick() {
     setCount(Count + 1);
   }
+
+  function handaleTotalClick() {
+    setTotal(Total+1);
+  }
+
+
   return (
     <div>
-      <h2>The count is: {Count}</h2>
       <button onClick={handaleClick}>
-        Click Me
+        Updated Count is: {Count}
+      </button>
+      <button onClick={handaleTotalClick}>
+        Updated Count is: {Total}
       </button>
 
     </div>
