b5f32f8f1356e7be4e5a0289f897292ac090c7e3
Thesachindey <sachinscode24@gmail.com>
2024-10-03 14:42:35 +0600

    add

---
+++ Lecture 09 useContext Hook in depth/src/App.jsx
@@ -0,0 +1,16 @@
+import { createContext } from 'react'
+import './App.css'
+
+function App() {
+  const userContext = createContext();
+
+  return (
+    <>
+      <div>
+        hello Sachin
+      </div>
+    </>
+  )
+}
+
+export default App
