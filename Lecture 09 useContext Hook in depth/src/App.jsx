ee9f37b0f456ea9dd81fff57ca97902960e317e9
Thesachindey <sachinscode24@gmail.com>
2024-10-04 13:46:37 +0600

    add

--- Lecture 09 useContext Hook in depth/src/App.jsx
+++ Lecture 09 useContext Hook in depth/src/App.jsx
@@ -1,27 +1,42 @@
-import { createContext,useState } from 'react'
+import { createContext, useState } from 'react'
 import './App.css'
 import ChildA from './components/ChildA';
 
 //Step1: create Context
-const AppContext = createContext();
+// const AppContext = createContext();
 //Step2: wrap all the child inside a "provider"
 //Step3: pass value
 //Step4: consumer yr vitory dhuky consume kora.
 
 
+const ThemeContext = createContext();
+
+
 function App() {
-const [User, setUser] = useState({name:"tim"});
+  // const [User, setUser] = useState({name:"tim"});
+
+  const [Theme, setTheme] = useState('Light');
+
 
   return (
-    <>
-      <AppContext.Provider value={User}>
-
+    <ThemeContext.Provider value={{Theme,setTheme}}>
+      <div id="container" style={{backgroundColor:Theme==='Light'?"beige":"black"}}>
         <ChildA />
+      </div>
+    </ThemeContext.Provider>
 
-      </AppContext.Provider>
-    </>
+
+
+    // <>
+    //   <AppContext.Provider value={User}>
+
+    //     <ChildA />
+
+    //   </AppContext.Provider>
+    // </>
   )
 }
 
 export default App
-export {AppContext}
+// export {AppContext}
+export { ThemeContext }
--- Lecture 09 useContext Hook in depth/src/components/ChildC.jsx
+++ Lecture 09 useContext Hook in depth/src/components/ChildC.jsx
@@ -1,11 +1,23 @@
-import React, { useContext } from 'react'
-import {AppContext} from '../App'
+import { useContext } from 'react'
+import { ThemeContext } from '../App'
 
 const ChildC = () => {
-    const user =useContext(AppContext);
+  // const user =useContext(AppContext);
+  const { Theme, setTheme } = useContext(ThemeContext);
+
+
+  function handleClick() {
+    if (Theme === 'Light') {
+      setTheme('Dark')
+    } else { setTheme('Light') }
+  }
+
+
   return (
     <div>
-      {user.name}
+      <button onClick={handleClick}>
+        Change Theme
+      </button>
     </div>
   )
 }
